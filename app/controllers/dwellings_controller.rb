3405bc65c0b40897b336b40130d1847aa07ba588
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-07-28 12:23:58 -0700

    FEATURE: added resources for dwellings, set up controller/actions, and views for all get actions

---
+++ app/controllers/dwellings_controller.rb
@@ -0,0 +1,10 @@
+class DwellingsController < ApplicationController
+	def index 
+	end
+	def show
+	end
+	def new
+	end
+	def edit
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :dwellings
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
